d20dcc5ec29b856257fa0602e0c45e91c4a35d82
dinel <solahuddin.hafid.tomaballo@gmail.com>
2022-03-23 22:07:10 +0800

    add lanjukang in route becouse nginx mutiple site

--- api/route.go
+++ api/route.go
@@ -15,36 +15,36 @@ func routes(app *config.AppConfig) http.Handler {
 	// make a recover midleware
 	// mux.Use(chiMiddleware.Recoverer)
 
-	r.HandlerFunc(http.MethodGet, "/", handlers.Repo.Home)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/", handlers.Repo.Home)
 
 	// route for static file
-	r.ServeFiles("/images/*filepath", http.Dir("images"))
+	r.ServeFiles("/lanjukang/images/*filepath", http.Dir("images"))
 
 	// user
-	r.HandlerFunc(http.MethodPost, "/user/signup", handlers.Repo.SignupHandler)
-	r.HandlerFunc(http.MethodPost, "/user/login", handlers.Repo.LoginHandler)
-	r.HandlerFunc(http.MethodPut, "/user/update-role", handlers.Repo.BecomeAdminHandler)
-	r.HandlerFunc(http.MethodPut, "/user/update-profile", handlers.Repo.UpdateUserHandler)
-	r.HandlerFunc(http.MethodGet, "/user/detail/:id", handlers.Repo.GetUserHandler)
-	r.HandlerFunc(http.MethodPost, "/user/forgot-password", handlers.Repo.ForgetPasswordHandler)
-	r.HandlerFunc(http.MethodPost, "/user/reset-password", handlers.Repo.ResetPasswordHandler)
-	r.HandlerFunc(http.MethodPut, "/user/image", handlers.Repo.UpdateUserImageHandler)
+	r.HandlerFunc(http.MethodPost, "/lanjukang/user/signup", handlers.Repo.SignupHandler)
+	r.HandlerFunc(http.MethodPost, "/lanjukang/user/login", handlers.Repo.LoginHandler)
+	r.HandlerFunc(http.MethodPut, "/lanjukang/user/update-role", handlers.Repo.BecomeAdminHandler)
+	r.HandlerFunc(http.MethodPut, "/lanjukang/user/update-profile", handlers.Repo.UpdateUserHandler)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/user/detail/:id", handlers.Repo.GetUserHandler)
+	r.HandlerFunc(http.MethodPost, "/lanjukang/user/forgot-password", handlers.Repo.ForgetPasswordHandler)
+	r.HandlerFunc(http.MethodPost, "/lanjukang/user/reset-password", handlers.Repo.ResetPasswordHandler)
+	r.HandlerFunc(http.MethodPut, "/lanjukang/user/image", handlers.Repo.UpdateUserImageHandler)
 
 	// service
-	r.HandlerFunc(http.MethodPost, "/service/create", handlers.Repo.CreateService)
-	r.HandlerFunc(http.MethodGet, "/service/list", handlers.Repo.ListAllService)
-	r.HandlerFunc(http.MethodGet, "/service/pop", handlers.Repo.ListPopService)
-	r.HandlerFunc(http.MethodGet, "/service/detail/:id", handlers.Repo.GetServiceDetail)
-	r.HandlerFunc(http.MethodPut, "/service/update/:id", handlers.Repo.UpdateService)
-	r.HandlerFunc(http.MethodDelete, "/service/delete/:id", handlers.Repo.DeleteService)
-	r.HandlerFunc(http.MethodGet, "/service/search", handlers.Repo.SeachService)
-	r.HandlerFunc(http.MethodGet, "/service/type/:id", handlers.Repo.ServiceByType)
+	r.HandlerFunc(http.MethodPost, "/lanjukang/service/create", handlers.Repo.CreateService)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/service/list", handlers.Repo.ListAllService)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/service/pop", handlers.Repo.ListPopService)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/service/detail/:id", handlers.Repo.GetServiceDetail)
+	r.HandlerFunc(http.MethodPut, "/lanjukang/service/update/:id", handlers.Repo.UpdateService)
+	r.HandlerFunc(http.MethodDelete, "/lanjukang/service/delete/:id", handlers.Repo.DeleteService)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/service/search", handlers.Repo.SeachService)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/service/type/:id", handlers.Repo.ServiceByType)
 
 	// Booking
-	r.HandlerFunc(http.MethodPost, "/booking/create", handlers.Repo.CreateBookingHandler)
-	r.HandlerFunc(http.MethodPut, "/booking/update", handlers.Repo.UpdateBookingHandler)
-	r.HandlerFunc(http.MethodGet, "/booking/list", handlers.Repo.GetBookingByUserHandler)
-	r.HandlerFunc(http.MethodDelete, "/booking/delete/:id", handlers.Repo.DeleteBookingHandler)
+	r.HandlerFunc(http.MethodPost, "/lanjukang/booking/create", handlers.Repo.CreateBookingHandler)
+	r.HandlerFunc(http.MethodPut, "/lanjukang/booking/update", handlers.Repo.UpdateBookingHandler)
+	r.HandlerFunc(http.MethodGet, "/lanjukang/booking/list", handlers.Repo.GetBookingByUserHandler)
+	r.HandlerFunc(http.MethodDelete, "/lanjukang/booking/delete/:id", handlers.Repo.DeleteBookingHandler)
 
 	return middleware.EnableCors(r)
 }
